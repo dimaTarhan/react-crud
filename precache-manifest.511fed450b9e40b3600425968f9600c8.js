f7defe31d8f7bc5d79981a37c05f49bc967eb478
Dmytro Tarhan <diditar.com@gmail.com>
2019-02-27 12:14:40 +0200

    Updates

---
+++ precache-manifest.511fed450b9e40b3600425968f9600c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e49355552c3b4fed99c3",
+    "url": "/react-crud/static/js/runtime~main.e4935555.js"
+  },
+  {
+    "revision": "4c6c0e0c29e6130b6977",
+    "url": "/react-crud/static/js/main.4c6c0e0c.chunk.js"
+  },
+  {
+    "revision": "b2754c5846a36300cd70",
+    "url": "/react-crud/static/js/2.b2754c58.chunk.js"
+  },
+  {
+    "revision": "4c6c0e0c29e6130b6977",
+    "url": "/react-crud/static/css/main.ff4e3319.chunk.css"
+  },
+  {
+    "revision": "6c7b322eea69563fc0711d800efa4342",
+    "url": "/react-crud/index.html"
+  }
+];
